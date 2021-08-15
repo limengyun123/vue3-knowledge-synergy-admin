002b81c908eec0ea213a3ca105e1db435f2d3f5c
limengyun <1832102745@qq.com>
2021-08-15 19:41:57 +0800

    dagre布局算法修改 以适应屏幕宽度

--- src/views/demo-antv/modify-layout.vue
+++ src/views/demo-antv/modify-layout.vue
@@ -1,51 +1,135 @@
 <template>
-  <div id="container"></div>
+  <div class="test-for-x6">
+    <div id="container1" style="background-color: pink"></div>
+    <div id="container2" style="background-color: pink"></div>
+  </div>
 </template>
 
 <script>
 import { defineComponent, onMounted } from "vue";
 import { Graph } from "@antv/x6";
-import { DagreLayout } from '@antv/layout'
+import { DagreLayout } from "@antv/layout";
 
 export default defineComponent({
   setup() {
-    onMounted(() => {
-      const graph = new Graph({
-        container: document.getElementById("container"),
-        width: 800,
-        height: 600,
-      });
-      graph.fromJSON(DagreLayout.layout(data));
-    });
+    onMounted(nodeNormal);
+    // onMounted(nodeSerial);
   },
 });
 
-const data = {
-  // 节点
-  nodes: [
-    {
-      id: "node1", // String，可选，节点的唯一标识
-      x: 40, // Number，必选，节点位置的 x 值
-      y: 40, // Number，必选，节点位置的 y 值
-      width: 80, // Number，可选，节点大小的 width 值
-      height: 40, // Number，可选，节点大小的 height 值
-      label: "hello", // String，节点标签
+
+function nodeNormal() {
+
+  const graph = new Graph({
+    container: document.getElementById("container1"),
+    width: 800,
+    height: 600,
+    scroller: {
+      enabled: true,
+      pannable: true,
+      pageVisible: true,
+      pageBreak: false,
     },
-    {
-      id: "node2", // String，节点的唯一标识
-      x: 160, // Number，必选，节点位置的 x 值
-      y: 180, // Number，必选，节点位置的 y 值
-      width: 80, // Number，可选，节点大小的 width 值
-      height: 40, // Number，可选，节点大小的 height 值
-      label: "world", // String，节点标签
+    mousewheel: {
+      enabled: true,
+      modifiers: ["ctrl", "meta"],
     },
-  ],
-  // 边
-  edges: [
-    {
-      source: "node1", // String，必须，起始节点 id
-      target: "node2", // String，必须，目标节点 id
+  });
+
+  const dagreLayout = new DagreLayout({
+    type: "dagre",
+    rankdir: "LR",
+    align: "UR",
+    ranksep: 35,
+    nodesep: 15,
+  });
+  const model = dagreLayout.layout(createNormalGraph());
+
+  graph.fromJSON(model);
+}
+
+
+  function nodeSerial(){
+    const graph = new Graph({
+      container: document.getElementById('container2'),
+      width: 800,
+      height: 600,
+      scroller: {
+      enabled: true,
+      pannable: true,
+      pageVisible: true,
+      pageBreak: false,
     },
-  ],
-};
+    mousewheel: {
+      enabled: true,
+      modifiers: ["ctrl", "meta"],
+    },
+  });
+
+    const dagreLayout = new DagreLayout({
+      type: "dagre",
+      rankdir: "LR",
+      align: "UR",
+      ranksep: 35,
+      nodesep: 15,
+    });
+    const model = dagreLayout.layout(createSerialGraph());
+
+    graph.fromJSON(model);
+
+  }
+
+
+function createNormalGraph() {
+  const nodeNum = 20;
+  let nodes = createNodes(nodeNum);
+
+  const edgeNum = 20;
+  let edges = [];
+
+  for (let i = 0; i < edgeNum; i++) {
+    let e1 = Math.floor(Math.random() * nodeNum);
+    let e2;
+    do {
+      e2 = Math.floor(Math.random() * nodeNum);
+    } while (e1 == e2);
+
+    edges.push({
+      source: e1.toString(),
+      target: e2.toString(),
+    });
+  }
+
+  return { nodes, edges };
+}
+
+function createSerialGraph() {
+  const nodeNum = 10;
+  let nodes = createNodes(nodeNum);
+
+  const edgeNum = 10;
+  let edges = [];
+
+  for (let i = 1; i < edgeNum; i++) {
+    edges.push({
+      source: (i-1).toString(),
+      target: i.toString(),
+    });
+  }
+
+  return { nodes, edges };
+}
+
+function createNodes(nodeNum){
+  let nodes = [];
+  for (let i = 0; i < nodeNum; i++) {
+    nodes.push({
+      id: i.toString(),
+      width: 80,
+      height: 40,
+      label: "节点 " + i,
+    });
+  }
+  return nodes;
+}
 </script>
