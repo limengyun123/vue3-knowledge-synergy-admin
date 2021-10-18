b5f02015bbc9f3ff01fa713ab6dd5af4f18e8646
limengyun <limengyun@sics.ac.cn>
2021-10-18 21:03:26 +0800

    属性过滤弹窗：完整的infinite scroll

--- src/views/demo-infinite-scroll/filter.vue
+++ src/views/demo-infinite-scroll/filter.vue
@@ -5,9 +5,10 @@
       <div style="padding: 8px; width: 390px; background-color: #ffffff; border-radius: 4px; box-shadow: 0 2px 8px #cccccc; ">
         <a-row style="padding: 9px" type="flex" justify="space-between">
           <a-checkbox
-            v-model:checked="selectResult.selectAll"
+            v-model:checked="checkAllState.selectAll"
+            :indeterminate="checkAllState.indeterminate"
+            @change="handleCheckAllClick($event.target.checked)"
           >
-            <!-- :indeterminate="dropdownIndeterminate" -->
             全选
           </a-checkbox>
           <span>
@@ -30,10 +31,10 @@
             <a-input placeholder="区间设置" />
           </a-col>
         </a-row>
-        <a-row style="height: 250px; overflow-y: scroll">
-          <div ref="targetDom" @click.capture="handleClick" :onscroll="handleScroll">
-            <div ref="targetTopDom" class="top-helper"></div>
-            <a-checkbox-group v-model:value="selectResult.selectedValues" style="background-color: lightgreen">
+        <a-row style="height: 250px;">
+          <div ref="targetDom" class="target-dom" :onscroll="handleScroll">
+            <div ref="targetDomTop" class="target-dom-top"></div>
+            <a-checkbox-group v-model:value="checkboxSelectedValues" style="background-color: lightgreen; text-align: left" @click.capture="handleCheckboxClick($event.target)">
               <div v-for="value in checkboxFixedValues" :key="value">
                 <a-checkbox :value="value" style="height: 30px;">{{value}}</a-checkbox>
               </div>
@@ -52,7 +53,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue';
+import { defineComponent, nextTick, onMounted, reactive, ref } from 'vue';
 import {
   FilterOutlined,
   RestOutlined,
@@ -60,50 +61,276 @@ import {
   SortDescendingOutlined,
   SwapRightOutlined
 } from "@ant-design/icons-vue";
-import testData from "@/assets/test.json";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const testData = require('../../assets/test.json');
+
+const requestData = (pageN: number)=>{
+  const pageSize = 30;
+  const itemTotalNum = testData.length;
+  // return Promise.reject();
+  return Promise.resolve({
+    total: itemTotalNum, 
+    results: testData.slice(Math.max((pageN-1)*pageSize,0), Math.min(pageN*pageSize, itemTotalNum))
+  });
+}
 
 export default defineComponent({
   components: {
-    FilterOutlined,
+    // FilterOutlined,
     RestOutlined,
     SortAscendingOutlined,
     SortDescendingOutlined,
     SwapRightOutlined
   },
   setup() {
-    const dropdownIconInactive = true;
-    const loading = ref(false);
-    const checkboxFixedValues = ref(testData);
-    const searchConditions = {
-      ascend: true,
-      searchContent: '',
-      intervalFrom: '',
-      intervalTo: ''
-    };
 
-    const selectResult = reactive({
-      selectAll: false,
-      selectedValues: []
-    });
+    const searchConditions = useSearchCondition();
+    const scrollControl = useScroll();
 
-
-    const handleClick = ()=>{};
-    const handleScroll = ()=>{};
     return {
-      dropdownIconInactive,
-      loading,
-      checkboxFixedValues,
-      searchConditions,
-      selectResult,
-      handleClick,
-      handleScroll
+      ...searchConditions,
+      ...scrollControl
     }
   },
 })
+
+function useSearchCondition(){
+  const dropdownIconInactive = true;
+  const searchConditions = {
+    ascend: true,
+    searchContent: '',
+    intervalFrom: '',
+    intervalTo: ''
+  };
+
+
+  return {
+    dropdownIconInactive,
+    searchConditions
+  }
+}
+
+type checkboxItemType = string|number|boolean|null;
+type checkboxItemsType = checkboxItemType[];
+
+function useScroll(){
+  const slotHeight = 30;
+  const loading = ref<boolean>(true);
+  const targetDom = ref(), targetDomTop = ref();
+  const checkboxFixedValues = ref<checkboxItemsType>([]), checkboxSelectedValues = ref<checkboxItemsType>([]);
+  let checkboxTrueValues: checkboxItemsType = [];
+  let cache: checkboxItemsType = [];
+  const checkAllState = reactive({
+    selectAll: false,
+    indeterminate: false
+  });
+
+  const pageInfo: {[key:string]:number} = {
+    pageNumber: 1,
+    pageSize: 30,
+    itemTotalNum: 0
+  }
+
+  let currentScrollTop, originScrollTop = 0;
+  let requestedDataType = '';
+
+  // pass
+  const handleCheckboxClick = (target: HTMLElement)=>{
+    if (target.nodeName!='INPUT' ) return ;
+    const val: checkboxItemType = target.getAttribute('value');
+    const index = checkboxTrueValues.indexOf(val);
+    if (index == -1) checkboxTrueValues.push(val);
+    else checkboxTrueValues.splice(index, 1);
+    updateCheckAllIcon();
+  };
+
+  // pass
+  const handleCheckAllClick = (val: boolean) => {
+    Object.assign(checkAllState, {selectAll: val, indeterminate: false });
+    checkboxTrueValues = [];
+    checkboxSelectedValues.value = val? [...checkboxFixedValues.value]: [];
+  };
+
+  // pass
+  const updateCheckAllIcon = () => {
+    const checkboxFakeValuesLength = checkboxTrueValues.length;
+
+    if (
+      !!checkboxFakeValuesLength &&
+      checkboxFakeValuesLength < pageInfo.itemTotalNum
+    ) {
+      checkAllState.indeterminate = true;
+      return;
+    }
+    if (checkboxFakeValuesLength == pageInfo.itemTotalNum) {
+      checkAllState.selectAll = !checkAllState.selectAll;
+      checkboxTrueValues = [];
+    }
+
+    checkAllState.indeterminate = false;
+  };
+
+  // 处理滚动事件
+  const handleScroll = () => {
+    currentScrollTop = targetDom.value.scrollTop;
+    const {pageNumber, pageSize, itemTotalNum} = pageInfo;
+
+    // 满足“下滚”更新数据的状态
+    // 四个条件逻辑分别表示：非正在请求数据、下滚、仍有数据可请求、初始进入下一个页码(请求数据的阶段)
+    if ( !loading.value && currentScrollTop > originScrollTop && pageNumber*pageSize < itemTotalNum && currentScrollTop >= (pageNumber-1) * pageSize * slotHeight ) {
+
+      // 读取缓存
+      if((pageNumber+1)*pageSize < cache.length){
+        console.log('cache', pageInfo.pageNumber+1);
+        const results = cache.slice(pageNumber*pageSize, (pageNumber+1)*pageSize);
+        cutOldDataScrollDown();
+        updateAfterRequested({results: results});
+        pageInfo.pageNumber += 1;
+        return ;
+      }
+
+      // 请求数据
+      console.log('request', pageNumber+1);
+      reqData(pageNumber + 1).then((res) => {
+        const { results } = res;
+        cutOldDataScrollDown();
+        updateAfterRequested({
+          results: results,
+          needCache: true
+        });
+        pageInfo.pageNumber += 1;
+      }).catch(()=>{
+        console.log('error');
+      }).finally(()=>{
+        loading.value = false;
+      });
+      
+    }
+
+    // 满足“上滚”更新数据的状态
+    // 四个条件逻辑分别表示：非正在请求数据、上滚、仍需要请求数据、初始进入上一个页码(请求数据的阶段)
+    if ( !loading.value && currentScrollTop < originScrollTop && pageNumber > 2 && currentScrollTop < (pageNumber - 2) * pageSize * slotHeight ) {
+      console.log('cache', pageInfo.pageNumber-2);
+      const results = pageNumber>3? cache.slice((pageNumber - 4)*pageSize, (pageNumber - 3)*pageSize): [];
+      // 先删除再插入
+      cutOldDataScrollUp();
+      updateAfterRequested({
+        results: results,
+        insertToHead: true
+      });
+      pageInfo.pageNumber -= 1;
+    }
+
+    originScrollTop = currentScrollTop;
+  };
+
+  // 下滚时裁剪旧数据
+  const cutOldDataScrollDown = ()=>{
+    const {pageNumber, pageSize} = pageInfo;
+
+    if (pageNumber > 2) {
+      targetDomTop.value.style.height = (pageNumber - 2) * pageSize * slotHeight + "px";
+      checkboxFixedValues.value.splice(0, pageSize);
+    }
+  };
+
+  // 上滚时裁剪旧数据
+  const cutOldDataScrollUp = ()=>{
+    const {pageNumber, pageSize} = pageInfo;
+
+    checkboxFixedValues.value.splice(2 * pageSize, pageSize);
+    pageNumber > 3 && (targetDomTop.value.style.height = (pageNumber - 4) * pageSize * slotHeight + "px");
+  }
+
+
+  // pass
+  const init = ()=>{
+    
+    reqData(pageInfo.pageNumber).then((res)=>{
+      const { results, total } = res;
+      pageInfo.itemTotalNum = total;
+      updateAfterRequested({results: results, needCache: true});
+
+    }).catch(()=>{
+      setTimeout(init, 2000);
+    });
+  };
+
+  const reqData = (pageNumber: number)=>{
+    loading.value = true;
+    return requestData(pageNumber);
+  }
+
+  // pass
+  const updateAfterRequested = (params: {results: checkboxItemsType, needCache?: boolean, insertToHead?: boolean})=>{
+    
+    const { needCache, insertToHead} = params;
+    let results = params.results;
+    // 更新数据缓存
+    if(needCache){
+      results = results.map((v)=>{
+        if(v===null) return v;
+        if(!requestedDataType) requestedDataType = typeof v; 
+        return v.toString();
+      })
+      cache = cache.concat(results||[])
+    }
+
+    // 更新checkbox group的项
+    insertToHead?
+      checkboxFixedValues.value.splice(0, 0, ...results) :
+      checkboxFixedValues.value.splice(checkboxFixedValues.value.length, 0, ...results);
+
+    // 更新checkbox group中已选择的项
+    nextTick(() => {
+      checkboxSelectedValues.value = checkAllState.selectAll?
+        checkboxFixedValues.value.filter((v)=>{
+          return checkboxTrueValues.indexOf(v)==-1;
+        }) :
+        [...checkboxTrueValues];
+    });
+    
+
+    // 更新加载中的状态
+    loading.value = false;
+  };
+
+  onMounted(()=>{
+    init();
+  });
+
+  
+
+  return {
+    targetDom,
+    targetDomTop,
+
+    checkAllState,
+    checkboxFixedValues,
+    checkboxSelectedValues,
+
+    handleCheckboxClick,
+    handleCheckAllClick,
+    handleScroll,
+  }
+}
 </script>
 
-<style lang="less">
+<style>
 .dropdown-icon-inactive{
   color: #aaaaaa;
+}
+
+.target-dom {
+  width: 374px;
+  height: 235px;
+  overflow: auto;
+  background-color: pink;
+}
+
+.target-dom-top {
+  width: 200px;
+  background-color: lightblue;
 }
 </style>
